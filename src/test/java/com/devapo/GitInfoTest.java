c76adadfd8ad55e8548eedc6b1d071ceb033b69e
admin <chernevskyymaks@gmail.com>
2020-02-24 12:51:03 +0100

    Project

---
+++ src/test/java/com/devapo/GitInfoTest.java
@@ -0,0 +1,12 @@
+package com.devapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitInfoTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
